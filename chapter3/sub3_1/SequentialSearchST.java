fa3cc18e5cca0b811ce8288c309bc581c05367eb
bitSeaChan <moldysaltedfish@gmail.com>
2021-03-15 11:45:01 +0800

    feat: add symbol table code

---
+++ chapter3/sub3_1/SequentialSearchST.java
@@ -0,0 +1,177 @@
+package chapter3.c3_1;
+
+import edu.princeton.cs.algs4.Queue;
+
+import java.util.Random;
+
+public class SequentialSearchST<Key, Value> {
+    private int n = 0;
+    private Node first = null;
+
+    // a helper linked list data type
+    private class Node {
+        Key key;
+        Value val;
+        Node next;
+
+        public Node(Key key, Value val, Node next) {
+            this.key = key;
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /**
+     * Initializes an empty symbol table
+     */
+    public SequentialSearchST() {
+    }
+
+    /**
+     * Returns the value associated with the given key in this symbol table.
+     *
+     * @param key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     * and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) return x.val;
+        }
+        return null;
+    }
+    
+    /**
+     * Inserts the specified key-value pair into the symbol table, overwriting the old
+     * value with the new value if the symbol table already contains the specified key.
+     * Deletes the specified key (and its associated value) from this symbol table
+     * if the specified value is {@code null}.
+     *
+     * @param key the key
+     * @param val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void put(Key key, Value val) {
+        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+
+        if (val == null) {
+            delete(key);
+            return;
+        }
+
+        // traverse linked list
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return;
+            }
+        }
+
+        // insert
+        first = new Node(key, val, first);
+        n++;
+    }
+
+    /**
+     * Returns the value associated with the given key in this symbol table.
+     *
+     * @param key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     * and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void delete(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
+        first = delete(first, key);
+    }
+
+    // delete key in linked list beginning at Node x
+    // warning: function call stack too large if table is large
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+        if (key.equals(x.key)) {
+            n--;
+            return x.next;
+        }
+        // recursion
+        x.next = delete(x.next, key);
+        return x;
+    }
+
+    /**
+     * Returns all keys in the symbol table as an {@code Iterable}.
+     * To iterate over all of the keys in the symbol table named {@code st},
+     * use the foreach notation: {@code for (Key key : st.keys())}.
+     *
+     * @return all keys in the symbol table
+     */
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<Key>();
+        for (Node x = first; x != null; x = x.next)
+            queue.enqueue(x.key);
+        return queue;
+    }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     *
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Returns true if this symbol table is empty.
+     *
+     * @return {@code true} if this symbol table is empty;
+     * {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * Returns true if this symbol table contains the specified key.
+     *
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key};
+     * {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
+    }
+
+    /**
+     * Generate a random string of the specified length
+     *
+     * @param length specified
+     * @return string
+     */
+    private static String generatorString(int length) {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        Random random = new Random();
+        return random.ints(leftLimit, rightLimit + 1)
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
+    // test case
+    public static void main(String[] args) {
+
+        SequentialSearchST<Integer, String> st = new SequentialSearchST<>();
+
+        for (int i = 0; i < 10; i++) {
+            st.put(i, generatorString(10));
+        }
+
+        for (int s : st.keys())
+            System.out.println(s + " " + st.get(s));
+    }
+}
